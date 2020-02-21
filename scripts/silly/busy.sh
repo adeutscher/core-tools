a7cb7a07535d6612172b1a363b63422ce1eab92c
Alan Deutscher <adeutscher@gmail.com>
2020-02-20 20:51:10 -0800

    Silly 'busy' script: Cut out the middleman by reading directly into 'hexdump' instead of using 'cat'.

--- scripts/silly/busy.sh
+++ scripts/silly/busy.sh
@@ -2,4 +2,4 @@
 
 # Make you look all busy and fancy in the eyes of non-technical people.
 # I forget the original author of this, probably someone on Reddit.
-cat /dev/urandom | hexdump -C | grep --color=auto "ca fe"
+hexdump -C /dev/urandom | grep --color=auto "ca fe"
