d8f03d696abbd0279895d3976528c02566fd49c9
Alan Deutscher <adeutscher@gmail.com>
2020-08-19 13:17:05 -0700

    random-command.sh: Apply shellcheck suggestions

--- scripts/system/random-command.sh
+++ scripts/system/random-command.sh
@@ -3,17 +3,15 @@
 # A silly script to print out random command names. Might help to inspire ideas.
 
 prep(){
-    rawTempFile=$(mktemp)
     filteredTempFile=$(mktemp)
 }
 
 close(){
-    rm -f "$rawTempFile"
     rm -f "$filteredTempFile"
 }
 
 # Handle arguments
-for opt in $(getopt ":s" $@); do
+for opt in $(getopt ":s" "$@"); do
     case "$opt" in
     "-s")
         # Enable stream mode
@@ -24,25 +22,25 @@ done
 prep
 
 # Gather commands.
-for path in $(echo $PATH | sed 's/\:/\ /g'); do
-    # Note: Using redirection of ls is much faster than BASH wildcards was.
-    for command in $(ls $path 2> /dev/null); do
-        echo $command >> $rawTempFile
+for path in ${PATH//:/ }; do
+    for command in "${path}"/*; do
+        if [ -x "${command}" ]; then
+            echo "${command##*/}"
+        fi
     done
-done
+done | sort | uniq > "$filteredTempFile"
 
-cat $rawTempFile | uniq | sort > $filteredTempFile
-rm $rawTempFile 2> /dev/null
 if (( "$stream" )); then
 
     trap close INT
 
-    cat $filteredTempFile | shuf -n1
-    while sleep 4; do
-        cat $filteredTempFile | shuf -n1
+    n=0
+    while sleep "${n}"; do
+        shuf -n1 < "${filteredTempFile}"
+        n=4
     done
 else
-    cat $filteredTempFile | shuf -n1
+    shuf -n1 < "${filteredTempFile}"
 fi
 
 close
