898a5e51108d81578aed090504adc398be6d4c42
Alan Deutscher <adeutscher@gmail.com>
2020-08-19 13:17:05 -0700

    toggle-touchpad.sh: Apply shellcheck suggestions

--- scripts/system/toggle-touchpad.sh
+++ scripts/system/toggle-touchpad.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Get the ID # of the touchpad
-device=$(xinput list| grep TouchPad | grep -om1 id\=[0-9]* | cut -d"=" -f 2)
+device=$(xinput list | grep TouchPad | grep -oPm1 "id\=[0-9]*" | cut -d"=" -f 2)
 # Double-check that a TouchPad exists.
 if [ -n "$device" ]; then
-   xinput list-props $device | grep "Device Enabled" | grep -q '1$'
+   xinput list-props "${device}" | grep "Device Enabled" | grep -q '1$'
    result=$?
-   xinput set-prop $device "Device Enabled" $result
+   xinput set-prop "${device}" "Device Enabled" $result
+else
+  # No TouchPad found...
+  exit 1
 fi
 
