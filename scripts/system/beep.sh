96a4170b65383d1a2f6548895e51bcf5bea8ddc2
Alan Deutscher <adeutscher@gmail.com>
2020-08-19 13:17:05 -0700

    beep.sh: Apply shellcheck suggestions

--- scripts/system/beep.sh
+++ scripts/system/beep.sh
@@ -4,7 +4,6 @@
 
 # Define colours
 if [ -t 1 ]; then
-  BLUE='\033[1;34m'
   GREEN='\033[1;32m'
   RED='\033[1;31m'
   YELLOW='\033[1;93m'
@@ -13,11 +12,11 @@ if [ -t 1 ]; then
 fi
 
 error(){
-  printf "$RED"'Error'"$NC"'['"$GREEN"'%s'"$NC"']: %s\n' "$(basename $0)" "$@"
+  printf "$RED"'Error'"$NC"'['"$GREEN"'%s'"$NC"']: %s\n' "$(basename "${0}")" "$@"
 }
 
 warning(){
-  printf "$YELLOW"'Warning'"$NC"'['"$GREEN"'%s'"$NC"']: %s\n' "$(basename $0)" "$@"
+  printf "$YELLOW"'Warning'"$NC"'['"$GREEN"'%s'"$NC"']: %s\n' "$(basename "${0}")" "$@"
   __warning_count=$((${__warning_count:-0}+1))
 }
 
@@ -29,24 +28,25 @@ beep(){
   # This function will fail quietly if we have no motherboard speaker for whatever reason.
   if [ -w "/dev/tty1" ]; then
     # Default number
-    local count=1
-    local count_limit=25
+    local count
+    count=1
+    local count_limit
+    count_limit=25
     if grep -Pq "^\d{1,}$" <<< "$1" ; then
-      if [ "$1" -le 0 ]; then
-        local count=1
-      elif [ "$1" -gt "$count_limit" ]; then
+      if [ "${1}" -gt "$count_limit" ]; then
         # Cap out at a certain number of beeps to prevent accidental typos.
         # If you really want to beep over 15 times, call beep multiple times
-        warning "$(printf "Capping beep count at ${BOLD}%s${NC}" "$count_limit")"
-        local count=$count_limit
-      else
-        local count=$1
+        warning "$(printf "Capping beep count at ${BOLD}%s${NC}" "${count_limit}")"
+        count=$count_limit
+      elif [ "${1}" -gt 0 ]; then
+        count=$1
       fi
     fi
-    local i=0
+    local i
+    i=0
     while [ "$i" -lt "$count" ]; do
       echo -e "\07" > /dev/tty1
-      local i=$(($i + 1))
+      i=$((i + 1))
       # Add a small beep to be able to tell individual beeps apart.
       sleep .1
     done
@@ -56,4 +56,4 @@ beep(){
   fi
 }
 
-beep $@
+beep "${1}"
