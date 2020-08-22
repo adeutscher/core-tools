ef2d15dccd23e2cc3fc53d22175031c45b55761e
Alan Deutscher <adeutscher@gmail.com>
2020-08-22 15:04:44 -0700

    shuffle-if.sh: Apply shellcheck suggestions

--- scripts/networking/shuffle-if.sh
+++ scripts/networking/shuffle-if.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-if=${1}
+interface="${1}"
 
 # Check to see if macchanger is installed.
-type macchanger 2> /dev/null >&2
-if [ "${?}" -gt 0 ]; then
+if ! type macchanger 2> /dev/null >&2; then
     echo "The 'macchanger' command is not installed on this system. Please install it before using this script."
     exit 1
 fi
 
-if [ -z "${if}" ]; then
+if [ -z "${interface}" ]; then
     echo "Please provide an interface."
     echo "Usage: ${0} iface"
     exit 2
 fi
 
-ifconfig ${if} 2>&1 > /dev/null
-if [ "${?}" -gt 0 ]; then
-    echo "Interface ${if} not found on system..."
+if ! ifconfig "${interface}" 2> /dev/null >&2; then
+    echo "Interface ${interface} not found on system..."
     exit 3
 fi
 
-if [ ${EUID} -ne 0 ]; then
+if (( EUID )); then
     echo "Not root, re-running script through sudo..."
-    sudo ${0} "${if}"
+    sudo "${0}" "${interface}"
     exit ${?}
 fi
 
-ifconfig ${if} down
-macchanger -a ${if}
-ifconfig ${if} up
+ifconfig "${interface}" down
+macchanger -a "${interface}"
+ifconfig "${interface}" up
 
