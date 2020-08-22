f2c4be7bdc38904d4c774e5dcf5bebbf0cfb269d
Alan Deutscher <adeutscher@gmail.com>
2020-08-22 15:04:44 -0700

    wifi-reset.sh: Apply shellcheck suggestions

--- scripts/networking/wifi-reset.sh
+++ scripts/networking/wifi-reset.sh
@@ -9,22 +9,23 @@
 #set -x
 
 # Monitoring wireless interface.
-iface=$1
+iface="${1}"
 
 # Exit if no interface given, or not actually a wireless interface
 [ -d "/sys/class/net/$iface/wireless/" ] || exit 1
 
 # Assumed to be broken until proven otherwise.
 # Crude check for the interface being up.
-if /sbin/ip a s $iface | /bin/grep -qw inet; then
+if /sbin/ip a s "${iface}" | /bin/grep -qw inet; then
+    # Can't just use (( EUID )) because sh doesn't support it
     if [ "$(id -u)" -ne 0 ]; then
         # If not running as root, re-run script as root.
         # Running this script on Raspbian, which does not ask for a password for sudo by default.
         # If you weren't on such a device, consider just running this script off of root's crontab instead.
-        sudo /bin/sh $0
+        sudo /bin/sh "${0}"
         exit 0
     fi
 
-    /sbin/ifdown $iface 2> /dev/null
-    /sbin/ifup $iface
+    /sbin/ifdown "${iface}" 2> /dev/null
+    /sbin/ifup "${iface}"
 fi
