e5fadc11369c92a0ef022df7355dace75c82dce7
Alan Deutscher <adeutscher@gmail.com>
2021-11-11 22:25:54 -0800

    Test Runner: Add basic filtering for faster test turnaround, mark some lines as not covered.

--- scripts/test/run.py
+++ scripts/test/run.py
@@ -7,7 +7,7 @@ DIRNAME = 'tests.d'
 DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), DIRNAME))
 sys.path.append('.')
 if not os.path.isdir(DIR):
-    raise Exception("Class dir doesn't exist: %s/" % DIR)
+    raise Exception("Class dir doesn't exist: %s/" % DIR) # pragma: no cover
 
 modules = []
 # Detect modules
@@ -27,14 +27,15 @@ for (dirname, subdirs, files) in os.walk(DIR):
 
 # Source: https://stackoverflow.com/questions/1732438/how-do-i-run-all-python-unit-tests-in-a-directory
 suite = unittest.TestSuite()
+filters = sys.argv[1:]
 
 for t in modules:
     mod = __import__(t, globals(), locals(), [DIR])
     # TODO: Implement a better method than checking the end of the class name.
     #         Best approach would be to check if unittest.TestCase is an ancestor of the class.
-    for test_class in [getattr(mod, tc) for tc in dir(mod) if tc.lower().endswith('tests')]:
+    for test_class_name, test_class in [(tc, getattr(mod, tc)) for tc in dir(mod) if tc.lower().endswith('tests')]:
         for method in dir(test_class):
-            if method.startswith('test_'):
+            if method.startswith('test_') and (not filters or [f for f in filters if f in method or f in test_class_name or f in t or f in '%s.%s' % (t, test_class_name)]):
                 suite.addTest(test_class(method))
 
 runner = unittest.TextTestRunner(verbosity = 2)
